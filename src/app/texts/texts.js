ef50a6aeb64a72d9c19bd5797537ca58a85c54cc
kevinpicon01 <digitalpruu@gmail.com>
2024-10-02 18:19:26 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,36 +1,38 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Latonería y Pintura";
+const email = "gomezcamilo750@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    `¡Hola! Estoy interesado en los servicios de ${title} y me gustaría obtener más información. ¡Gracias!`
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "3172803915";
 const textos = {
     index: {
-        title: title,
+        title: `${title}`,
+        slogan: "Siempre a tu servicio",
     },
     homeContent: {
-        title: title,
+        title: `${title}`,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "En Latonería y Pintura, nos especializamos en ofrecer servicios de alta calidad en latonería y pintura. Con años de experiencia en el sector, nuestro objetivo es proporcionar resultados precisos y duraderos que cumplan con los más altos estándares de calidad. \n" +
+            "Nos enorgullece ofrecer un servicio detallado y meticuloso en cada proyecto. Desde reparaciones de latonería hasta trabajos de pintura, cada tarea es realizada con cuidado y profesionalismo para asegurar que el resultado final sea impecable. \n" +
+            "En Latonería y Pintura, entendemos la importancia de la precisión y la calidad en cada trabajo. Ya sea que necesites reparar daños en la carrocería de tu vehículo o darle un nuevo acabado a tus espacios, nuestro equipo está listo para superar tus expectativas. \n" +
+            "Confía en Latonería y Pintura para obtener resultados excepcionales en todos tus proyectos de latonería y pintura. Estamos comprometidos con la satisfacción del cliente y la excelencia en cada detalle.  \n"
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "",
+            p2: "Siempre a tu servicio",
         },
-        address: "Dirección del emprendimiento",
+        address: "",
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
-        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+        facebook: "PENDIENTE en crear",
+        instagram: "No",
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
     },
-
 };
 export default textos;
